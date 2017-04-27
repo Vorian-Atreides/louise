236b7ccdc1b375ead87b77362c00ac11e87eecd5
Gaston Siffert <siffer_g@Gastons-MacBook-Pro.local>
2017-04-27 16:40:36 +0200

    Version of 27 April 2017

---
+++ www/js/script.js
@@ -0,0 +1,15 @@
+function show() {
+    document.getElementById('video').style.visibility = 'visible';
+    document.getElementById('minus').style.visibility = 'visible';
+    document.getElementById('plus').style.visibility = 'hidden';
+}
+
+function hide() {
+    document.getElementById('video').style.visibility = 'hidden';
+    document.getElementById('minus').style.visibility = 'hidden';
+    document.getElementById('plus').style.visibility = 'visible';
+}
+
+function sayHello() {
+   alert("Hello World")
+}
